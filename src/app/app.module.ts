82a168e4a3b08d24c26e61a879b67282b4dca18a
yuanyu90221 <yuanyu90221@gmail.com>
2019-05-12 23:29:34 +0800

    modify nested component make the default test pass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/components/header/header.component';
+import { HomesComponent } from './src/components/homes/homes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomesComponent
   ],
   imports: [
     BrowserModule
