59be257b049b4be4a7d2cfd53811227337f4ff38
yuanyu90221 <yuanyu90221@gmail.com>
2019-05-12 23:56:33 +0800

    add header component

--- src/app/src/components/header/header.component.spec.ts
+++ src/app/src/components/header/header.component.spec.ts
@@ -19,7 +19,24 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show logo', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="logo"]')).toBeTruthy();
+  });
+
+  it('should show search', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="search"]')).toBeTruthy();
+  });
+
+  it('should show menu', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="menu"]')).toBeTruthy();
+  });
+
+  it('should show filter', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="home-type"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="dates"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="guests"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="price"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="rooms"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="amenities"]')).toBeTruthy();
   });
 });
